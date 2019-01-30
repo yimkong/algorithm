102b10c1b763934e7f44489d970ae035f3bd12bc
yg <17665448577@163.com>
2019-01-30 19:45:02 +0800

    thread interview question

---
+++ src/main/java/thread/NotifyAndWait.java
@@ -0,0 +1,55 @@
+package thread;
+
+/**
+ * author yg
+ * description 启动两个线程,一个输出1,3,5,7...99,另一个输出2,4,6,8...100,最后STOUT按序输出1,2,3...100
+ * date 2019/1/30
+ */
+public class NotifyAndWait {
+    static Object obj = new Object();
+    static volatile int i = 1;
+
+    public static void main(String[] args) {
+
+        Thread thr1 = new Thread(new Runnable() {
+            public void run() {
+                while (i <= 100) {
+                    synchronized (obj) {
+                        if (i % 2 != 0) {
+                            System.err.println(Thread.currentThread().getName() + ":" + i++);
+                            obj.notify();
+                        } else {
+                            try {
+                                obj.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        Thread thr2 = new Thread(new Runnable() {
+            public void run() {
+                while (i <= 100) {
+                    synchronized (obj) {
+                        if (i % 2 == 0) {
+                            System.err.println(Thread.currentThread().getName() + ":" + i++);
+                            obj.notify();
+                        } else {
+                            try {
+                                obj.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        thr1.start();
+        thr2.start();
+
+    }
+
+}
