8d9dbac9dd4e6512cac35ac0a4589ffb587de1e4
yimkong <17665448577@163.com>
2019-10-30 10:52:05 +0800

    data structure

---
+++ src/main/java/dataStructure/Test.java
@@ -0,0 +1,39 @@
+package dataStructure;
+
+/**
+ * author yg
+ * description
+ * date 2019/10/30
+ */
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a","c");
+        map.put("a","4");
+        map.put("9","1");
+        map.put("1","1");
+        map.put("2","1");
+        map.put("3","1");
+        map.put("4","1");
+        map.put("5","1");
+        map.put("12f","1");
+        map.put("asdf","1");
+        map.put("asd","1");
+        map.put("k","1");
+        map.put("z","1");
+        map.put("bf","1");
+        map.put("zb","1");
+        map.put("cbf","1");
+        map.put("badf","1");
+        map.put("bbf","1");
+        map.put("bzf","1");
+        map.put("basdzf","1");
+        map.put("bzaf","1");
+        map.put("badzaf","1");
+        map.put("bzaff","1");
+        map.put("bzvyaf","1");
+        map.put("bzdvyaf","1");
+        map.put("bzvayaf","1");
+        System.err.println(map);
+    }
+}
