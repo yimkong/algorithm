388aeb7b43241989ee24b61ae6ae9b27d6559f95
yimkong <17665448577@163.com>
2020-03-22 10:57:52 +0800

    modify note

--- src/main/java/sort/QuitSort.java
+++ src/main/java/sort/QuitSort.java
@@ -4,7 +4,7 @@ package sort;
  * author yg
  * description 快速排序
  * 必须从右边开始找,是因为必须先找到比基准小的数,基准交换才准确(左边的全部比中间基准小,右边的全部比中间基准大)
- * 如果先从左边开始找,找到比基准大的,交换后左边的原基准位还是比中间基准大,不正确
+ * 如果先从左边开始找,找到比基准大的,有可能交换后左边的原基准位还是比中间基准大,不正确
  * date 2019/2/4
  */
 public class QuitSort {
@@ -38,6 +38,7 @@ public class QuitSort {
         qsort(arr, i + 1, right);
     }
 
+    //将数组中的俩个位置的数交换位置
     private static void change(int[] arr, int i, int j) {
         //交换
         int temp = arr[i];
