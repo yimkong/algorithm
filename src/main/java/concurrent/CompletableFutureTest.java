c65fb2cc4c5a45c7d81c6c3a9277607760b6dce3
yimkong <17665448577@163.com>
2020-11-07 09:41:21 +0800

    add CompletableFuture test

--- src/main/java/concurrent/CompletableFutureTest.java
+++ src/main/java/concurrent/CompletableFutureTest.java
@@ -1,6 +1,7 @@
 package concurrent;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
 
 /**
@@ -9,7 +10,7 @@ import java.util.function.Function;
  * date 2020/11/7
  */
 public class CompletableFutureTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         CompletableFutureTest completableFutureTest = new CompletableFutureTest();
         completableFutureTest.test();
         completableFutureTest.test();
@@ -21,6 +22,22 @@ public class CompletableFutureTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        completableFutureTest.test2();
+        completableFutureTest.test3();
+    }
+
+    private void test3() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> illegal = CompletableFuture.supplyAsync(() -> Integer.parseInt("ILLEGAL"))
+                .thenApply(r -> r * 2 * Math.PI)
+                .thenApply(s -> "apply>>" + s)
+                .exceptionally(ex -> "Error:" + ex.getMessage());
+        System.err.println(illegal.get());
+    }
+
+    private void test2() {
+        //多个并发函数组合并join入主线程得到结果
+        String join = CompletableFuture.supplyAsync(() -> "hello").thenCombine(CompletableFuture.supplyAsync(() -> "world"), (s1, s2) -> s1 + " " + s2).join();
+        System.err.println(join);
     }
 
     private void test() {
