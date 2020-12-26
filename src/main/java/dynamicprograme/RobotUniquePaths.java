7c828a8f3b8fdc6187e047399ec3eb9742754755
yimkong <17665448577@163.com>
2020-12-26 11:37:22 +0800

    add 1 problem

---
+++ src/main/java/dynamicprograme/RobotUniquePaths.java
@@ -0,0 +1,80 @@
+package dynamicprograme;
+
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * <p>
+ * 问总共有多少条不同的路径？
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：m = 3, n = 7
+ * 输出：28
+ * 示例 2：
+ * <p>
+ * 输入：m = 3, n = 2
+ * 输出：3
+ * 解释：
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向右 -> 向下
+ * 2. 向右 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向右
+ * 示例 3：
+ * <p>
+ * 输入：m = 7, n = 3
+ * 输出：28
+ * 示例 4：
+ * <p>
+ * 输入：m = 3, n = 3
+ * 输出：6
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= m, n <= 100
+ * 题目数据保证答案小于等于 2 * 109
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-paths
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RobotUniquePaths {
+    //空间复杂度O(m*n)
+    public int uniquePaths(int m, int n) {
+        if (m <= 1 || n <= 1) {
+            return 1;
+        }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m - 1][n - 2] + dp[m - 2][n - 1];
+    }
+
+    //优化空间复杂度 为O(n)
+    public int uniquePaths1(int m, int n) {
+        if (m <= 1 || n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0]=1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] = dp[j - 1] + dp[j];
+            }
+        }
+        return dp[n - 1];
+    }
+}
