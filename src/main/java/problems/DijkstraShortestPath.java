3a8e3c44e97cef141b059b42fb47e293e32f4824
yimkong <17665448577@163.com>
2020-12-28 20:14:17 +0800

    fix up bug

--- src/main/java/problems/DijkstraShortestPath.java
+++ src/main/java/problems/DijkstraShortestPath.java
@@ -7,23 +7,22 @@ public class DijkstraShortestPath {
     /**
      * Dijkstra 算法算最短路径
      *
-     * @param startNode 开始的节点
+     * @param startMyNode 开始的节点
      * @return 距离开始的节点最近的节点
      */
-    public static MyNode shortestPath(MyNode startNode) {
-        //已访问列表
+    public static MyNode shortestPath(MyNode startMyNode) { //已访问列表
         List<MyNode> visited = new ArrayList<>();
         //未访问列表
         List<MyNode> unvisited = new ArrayList<>();
         //初始化unvisited列表的数据
-        unvisited.add(startNode);
-        Set<MyNode> toAddSet = new HashSet<>(startNode.getAdjacentNodes().keySet());
+        unvisited.add(startMyNode);
+        Set<MyNode> toAddSet = new HashSet<>(startMyNode.getAdjacentMyNodes().keySet());
         boolean empty = toAddSet.isEmpty();
         while (!empty) {
             unvisited.addAll(toAddSet);
             HashSet<MyNode> hashSet = new HashSet<>();
-            for (MyNode node : toAddSet) {
-                hashSet.addAll(node.getAdjacentNodes().keySet());
+            for (MyNode MyNode : toAddSet) {
+                hashSet.addAll(MyNode.getAdjacentMyNodes().keySet());
             }
             toAddSet.addAll(hashSet);
             //防止死循环
@@ -31,52 +30,56 @@ public class DijkstraShortestPath {
             empty = toAddSet.isEmpty();
         }
         Map<Character, MyNode> table = new HashMap<>();
-        startNode.setD(0);
-        //初始化table
+        startMyNode.setD(0);
+        //初始化table 里面存的是每一次访问节点时，每个节点距离开始节点最近的距离长度，初始时为 Integer.MAX_VALUE
         unvisited.forEach(x -> table.put(x.getName(), x));
-        MyNode pre = startNode;
-        while (pre != null) {
-            Map.Entry<MyNode, Integer> minEntry = null;//每一次访问节点到关联的最短距离的下一个节点记录
-            for (Map.Entry<MyNode, Integer> entry : pre.getAdjacentNodes().entrySet()) {
+        //访问节点
+        MyNode pre = startMyNode;
+        while (true) {
+            MyNode minMyNode = null;//当前pre节点的所有关联节点中，距离开始节点最近的节点entry
+            unvisited.remove(pre);
+            visited.add(pre);
+            for (Map.Entry<MyNode, Integer> entry : pre.getAdjacentMyNodes().entrySet()) {
                 char name = entry.getKey().getName();
-                MyNode recordNode = table.get(name);
-                if (visited.contains(recordNode)) {
+                MyNode recordMyNode = table.get(name);
+                if (visited.contains(recordMyNode)) {
                     continue;
                 }
                 Integer curDistance = entry.getValue();
-                //该节点到上一个节点的距离比记录的小，更新table里的recordNode
-                if (curDistance < recordNode.getD()) {
-                    recordNode.setD(curDistance);
-                    recordNode.setPi(pre);
+                //该关联节点加上上一个节点的距离，如果比记录的小，则更新table里的recordMyNode 并记录最短距离的有效上一个节点为当前Pre访问节点
+                if (curDistance + pre.getD() < recordMyNode.getD()) {
+                    recordMyNode.setD(curDistance + pre.getD());
+                    recordMyNode.setPi(pre);
                 }
-                if (minEntry == null) {
-                    minEntry = entry;
+                if (minMyNode == null) {
+                    minMyNode = recordMyNode;
                     continue;
                 }
-                if (recordNode.getD() < minEntry.getValue()) {
-                    minEntry = entry;
+                if (recordMyNode.getD() < minMyNode.getD()) {
+                    minMyNode = recordMyNode;
                 }
             }
-            unvisited.remove(pre);
-            visited.add(pre);
-            if (minEntry == null) {
+            if (minMyNode == null) {
                 break;
             }
-            pre = minEntry.getKey();
+            pre = minMyNode;
         }
-        //最后遍历一遍得到距离startNode最近的节点
-        MyNode minNode = null;
-        table.remove(startNode.getName());
+        //最后遍历一遍得到距离startMyNode最近的节点
+        MyNode minMyNode = null;
         for (Map.Entry<Character, MyNode> entry : table.entrySet()) {
-            if (minNode == null) {
-                minNode = entry.getValue();
+            MyNode MyNode = entry.getValue();
+            if (MyNode == startMyNode) {
+                continue;
+            }
+            if (minMyNode == null) {
+                minMyNode = MyNode;
             } else {
-                if (entry.getValue().getD() < minNode.getD()) {
-                    minNode = entry.getValue();
+                if (MyNode.getD() < minMyNode.getD()) {
+                    minMyNode = entry.getValue();
                 }
             }
         }
-        return minNode;
+        return minMyNode;
     }
 }
 
@@ -84,7 +87,7 @@ class MyNode {
     char name;//对应的节点
     int d;//当前记录最短距离
     MyNode pi;//最短距离的上一个节点
-    Map<MyNode, Integer> adjacentNodes = new HashMap<>();//相连的其他节点以及对应的距离
+    Map<MyNode, Integer> adjacentMyNodes = new HashMap<>();//相连的其他节点以及对应的距离
 
     public MyNode(char name) {
         this.name = name;
@@ -116,15 +119,15 @@ class MyNode {
         this.pi = pi;
     }
 
-    public Map<MyNode, Integer> getAdjacentNodes() {
-        return adjacentNodes;
+    public Map<MyNode, Integer> getAdjacentMyNodes() {
+        return adjacentMyNodes;
     }
 
-    public void setAdjacentNodes(Map<MyNode, Integer> adjacentNodes) {
-        this.adjacentNodes = adjacentNodes;
+    public void setAdjacentMyNodes(Map<MyNode, Integer> adjacentMyNodes) {
+        this.adjacentMyNodes = adjacentMyNodes;
     }
 
-    public void addDestination(MyNode nodeB, int i) {
-        adjacentNodes.put(nodeB, i);
+    public void addDestination(MyNode MyNodeB, int i) {
+        adjacentMyNodes.put(MyNodeB, i);
     }
 }
