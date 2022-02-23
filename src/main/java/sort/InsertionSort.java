5573498119baa96b647f0d5ec6485461fca67bc0
yimkong <351072414@qq.com>
2022-02-23 19:34:11 +0800

    add insertion sort

---
+++ src/main/java/sort/InsertionSort.java
@@ -0,0 +1,25 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int a[] = {8, 2, 3, 1, 6, 2, 3};
+        sort(a);
+        String s = Arrays.toString(a);
+        System.err.println(s);
+    }
+
+    private static void sort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int j = i;
+            while (j > 0 && a[j] < a[j-1]){
+                int temp = a[j];
+                a[j] = a[j -1];
+                a[j-1] = temp;
+                --j;
+            }
+
+        }
+    }
+}
