6b0cecb71c0828d48baf73a0db694b5c9bf489fb
yimkong <351072414@qq.com>
2022-02-27 18:40:38 +0800

    add Semaphore usage

---
+++ src/main/java/thread/TestSemaphore.java
@@ -0,0 +1,34 @@
+package thread;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 2022/2/27
+ * desc
+ */
+public class TestSemaphore {
+    static Semaphore semaphore = new Semaphore(5);
+    static AtomicInteger a = new AtomicInteger();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        try{
+                            System.err.println("yes i am "+ a.getAndIncrement());
+                            Thread.sleep(5000);
+                        }finally {
+                            semaphore.release();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}
