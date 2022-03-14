fbbed5cfee4da1016950d65a424dda0e1216b784
yimkong <351072414@qq.com>
2022-03-14 22:48:17 +0800

    upgrade dependencies

--- src/main/java/hotswap/Test.java
+++ src/main/java/hotswap/Test.java
@@ -1,7 +1,5 @@
 package hotswap;
 
-import org.apache.log4j.PropertyConfigurator;
-
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -12,13 +10,13 @@ import java.util.Scanner;
  */
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
-        PropertyConfigurator.configure(new Test().getClass().getClassLoader().getResource("log4j.properties").getPath());
-        SimpleRunner simpleRunner = new SimpleRunner();
-        simpleRunner.run();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String s = scanner.nextLine();
-            simpleRunner.run();
-        }
+//        PropertyConfigurator.configure(new Test().getClass().getClassLoader().getResource("log4j.properties").getPath());
+//        SimpleRunner simpleRunner = new SimpleRunner();
+//        simpleRunner.run();
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            String s = scanner.nextLine();
+//            simpleRunner.run();
+//        }
     }
 }
