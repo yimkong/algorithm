c054fd18b2faa41ad14c333f9155a3f5b8abcbdc
ryan <ryangyan@163.com>
2023-08-19 20:14:22 +0800

    update content

--- src/main/java/problems/problem104.py
+++ src/main/java/problems/problem104.py
@@ -11,12 +11,6 @@ DFS ：用递归
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         return self.go(root, 0)
