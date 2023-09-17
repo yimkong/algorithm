e625822e58b219dc7608d62280cb8438b7018fc3
ryan <ryangyan@163.com>
2023-09-17 17:33:27 +0800

    optimize code

--- src/main/java/problems/Problem338.py
+++ src/main/java/problems/Problem338.py
@@ -17,3 +17,12 @@ class Solution:
                 ans.append(count + 1)
                 dic[i] = count + 1
         return ans
+# 简化版本
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n == 0: return [0]
+        ans = []
+        ans.append(0)
+        for i in range(1, n + 1):
+            ans.append(ans[i & i - 1] + 1)
+        return ans
